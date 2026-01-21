336191950335720239477966d4860b48fa4ff917
ShimiManashirov <122288727+ShimiManashirov@users.noreply.github.com>
2026-01-21 11:59:32 +0200

    Auth function (#3)
    * Refactor project structure to TypeScript
    
    - Converted JavaScript files to TypeScript in the src directory.
    - Created new models for comments and posts using TypeScript.
    - Implemented controllers for handling comments and posts.
    - Updated routes to use TypeScript and modular imports.
    - Removed old JavaScript files and updated package.json for TypeScript support.
    - Added TypeScript configuration and build scripts.
    - Fixed typos in request examples.
    
    * auth functions

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import postRoute from "./routes/postRoute";
 import commentRoute from "./routes/commentRoute";
+import authRoute from "./routes/authRoute";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 const app: Express = express();
 
 app.use(express.json());
 app.use("/posts", postRoute);
 app.use("/posts/:postId/comments", commentRoute);
+app.use("/auth", authRoute);
+
 
 const initApp = async (): Promise<Express> => {
   try {
